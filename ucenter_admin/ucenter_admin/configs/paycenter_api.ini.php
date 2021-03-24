1ca0a754749c7503f1c0df52011c22ea734acf89
chenyang <tech127@yuanfeng.cn>
2021-03-24 17:36:19 +0800

    Signed-off-by: chenyang <tech127@yuanfeng.cn> 项目3

---
+++ ucenter_admin/ucenter_admin/configs/paycenter_api.ini.php
@@ -0,0 +1,7 @@
+<?php
+$paycenter_api_key = 'LlbnMt4T';
+$paycenter_api_url = 'http://sh.pay.com/';
+$paycenter_app_id = 105;
+$paycenter_admin_api_url = 'http://sh.payadmin.com/';
+$paycenter_admin_url = 'http://sh.payadmin.com/';
+$paycenter_api_name = '网付宝';
