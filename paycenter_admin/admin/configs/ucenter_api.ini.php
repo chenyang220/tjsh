ed0967b10966a6df06e131b45272b30edc391de6
chenyang <tech127@yuanfeng.cn>
2021-03-24 17:38:30 +0800

    Signed-off-by: chenyang <tech127@yuanfeng.cn> 项目4

---
+++ paycenter_admin/admin/configs/ucenter_api.ini.php
@@ -0,0 +1,6 @@
+<?php
+$ucenter_api_key = 'LlbnMt4T';
+$ucenter_api_url = 'http://sh.ucenter.com/';
+$ucenter_app_id = 104;
+$ucenter_admin_url = 'http://sh.ucenteradmin.com/';
+$ucenter_admin_api_url = 'http://sh.ucenteradmin.com/';
